9c4405f18efe9bc3202c5f54bda472df234b7389
mwunnam <mwunnam@gmail.com>
2024-07-25 23:13:34 +0000

    done with task 0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+''' Basic_cache '''
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    def put(self, key, item):
+        ''' Add an item to the cache '''
+        if key is None or item is None:
+            return None
+        self.cache_data[key] = item
+
+    def get(self, key):
+        ''' Get an item by key from the cache '''
+        if key is None or key is not in self.cache.data:
+            return None
+        return self.cache_data[key]
