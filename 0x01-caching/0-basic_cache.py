17841fd544602afadb272ba1c4245e780c963116
mwunnam <mwunnam@gmail.com>
2024-07-26 20:23:41 +0000

    correction on task 0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -5,14 +5,14 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
-    def put(self, key, item):
+    def put(self, key: str, item: any) -> None:
         ''' Add an item to the cache '''
-        if key is None or item is None:
+        if key is None and item is None:
             return None
         self.cache_data[key] = item
 
-    def get(self, key):
+    def get(self, key: str) -> any:
         ''' Get an item by key from the cache '''
         if key is None or key not in self.cache_data:
             return None
-        return self.cache_data[key]
+        return self.cache_data.get(key, None)
