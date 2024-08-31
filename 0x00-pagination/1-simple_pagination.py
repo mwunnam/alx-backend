458088a9916b2005eee560265424d40b94a7f8d0
mwunnam <mwunnam@gmail.com>
2024-08-31 23:17:10 +0000

    correection on task 1

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -40,10 +40,8 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert isinstance(page, int) and page > 0, "Page must be an integer
-        and greater than 0"
-        assert isinstance(page_size, int) and page_size > 0, "Page size must
-        be an integer and greated than 0"
+        assert isinstance(page, int) and page > 0, "Page must be an integer and greater than 0"
+        assert isinstance(page_size, int) and page_size > 0, "Page size must be an integer and greated than 0"
 
         start_index, end_index = index_range(page, page_size)
 
