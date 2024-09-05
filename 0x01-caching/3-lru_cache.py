1802272d7e31422c2984372b2862b5c6836be6ad
mwunnam <mwunnam@gmail.com>
2024-09-04 17:56:53 -0700

    correction on task 3

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -15,16 +15,23 @@ class LRUCache(BaseCaching):
         if key is None or item is None:
             return
 
+        if key in self.cache_data:
+            self.cache_order.remove(key)
+
+        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            lru = self.cache_order.pop(0)
+            del self.cache_data[lru]
+            print(f'DISCARD: {lru}')
+
         self.cache_data[key] = item
         self.cache_order.append(key)
 
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            lru = self.cache_order.pop()
-            print(f'DISCARD: {lru}')
-            if lru in self.cache_data:
-                del self.cache_data[lru]
-
     def get(self, key):
         """Get item from the cache"""
-        if key is not None and key in self.cache_data:
-            return self.cache_data.get(key, None)
+        if key is None and key not in self.cache_data:
+            return None
+
+        if key in self.cache_order:
+            self.cache_order.remove(key)
+            self.cache_order.append(key)
+        return self.cache_data.get(key)
