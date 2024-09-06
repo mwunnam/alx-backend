847728385d20a2bec5f1f2a6d14fe291e9fe69bc
mwunnam <mwunnam@gmail.com>
2024-09-06 16:29:01 +0000

    done with task 3

---
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+import math
+from typing import List
+from typing import Dict
+from typing import Any
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """This returns data with respect to the deleted records"""
+
+        assert index is not None, "index is out of range"
+        assert index >= 0, "index is out of range"
+        assert index >= len(self.indexed_dataset()), "index is out of range"
+
+        data = []
+        current_index = index
+        dataset = self.indexed_dataset()
+
+        while len(data) < page_size and current_index < len(dataset):
+            if current_index in dataset:
+                data.append(dataset[current_index])
+            current_index += 1
+
+        next_index = current_index if current_index < len(dataset) else None
+
+        return {
+               "index": index,
+               "next_index": next_index,
+               "page_size": len(data),
+               "data": data,
+           }
