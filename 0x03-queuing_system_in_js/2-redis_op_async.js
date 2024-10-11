b4ebe2fb3ef67fb14161fbdc6a748d7f704f04c6
mwunnam <mwunnam@gmail.com>
2024-10-11 21:47:46 +0000

    adding more files

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,33 @@
+import { createClient, print } from 'redis';
+import { promisify } from 'util';
+
+const client = createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err}`);
+});
+
+const getAsync = promisify(client.get).bind(client);
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, print);
+}
+
+async function displaySchoolValue(schoolName) {
+  try {
+    const value = await getAsync(schoolName);
+    console.log(value);
+  } catch (err) {
+      console.log(err);
+  }
+}
+
+(async () => {
+  await displaySchoolValue('Holberton');
+  setNewSchool('HolbertonSanFrancisco', '100');
+  await displaySchoolValue('HolbertonSanFrancisco');
+})();
