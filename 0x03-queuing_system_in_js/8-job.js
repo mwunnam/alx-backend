9a69b7b21217aaf4e1c5b914cb3486e67016e9c8
mwunnam <mwunnam@gmail.com>
2024-10-11 23:57:23 +0000

    done with task 10

---
+++ 0x03-queuing_system_in_js/8-job.js
@@ -0,0 +1,32 @@
+import kue from 'kue';
+
+const queue = kue.createQueue();
+
+function createPushNotificationsJobs(jobs, queue) {
+  if (!Array.isArray(jobs)) {
+    throw new Error('Job is not an array');
+  }
+
+  jobs.forEach((jobData) => {
+    const job = queue.create('push_notification_code_3', jobData)
+      .save((err) => {
+        if (!err) {
+          console.log(`Notification job created: ${job.id}`);
+        }
+      });
+
+    job.on('complete', () => {
+      console.log(`Notification job ${job.id} completed`);
+    });
+
+    job.on('failed', (err) => {
+      console.log(`Notification job ${job.id} failed`);
+    });
+
+    job.on('progress', (progress) => {
+      console.log(`Notification job ${job.id} ${progress}% complete`);
+    });
+  });
+}
+
+export default createPushNotificationsJobs;
